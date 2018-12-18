2a4be4c3a9e4fb429dd44586f0e74c9a93994f8a
Buhanov-Dima <35217770+Buhanov-Dima@users.noreply.github.com>
2018-12-18 22:45:57 +0300

    fx-favicon

--- index.php
+++ index.php
@@ -2,27 +2,29 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>test</title>
+	<title>Van Der Q</title>
 	<link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
-	<header>
-		<div class="cnt">
-			<div class="logo">
-				<img src="img/logo.png" alt="">
-			</div>
-			<div class="main_menu">
-				<ul class="menu">
-					<a href="#"><li>Взгляд</li></a>
-					<a href="#"><li>Вкус</li></a>
-					<a href="#"><li>Звук</li></a>
-					<a href="#"><li>Ощущения</li></a>
-					<a href="#"><li>Запах</li></a>
-					<a href="#"><li>Интуиция</li></a>
-				</ul>
-			</div>
+<header>
+	<div class="cnt">
+		<div class="logo">
+			<img src="img/logo.png" alt="">
 		</div>
-	</header>
+	</div>
+	<div class="main_menu">
+		<div class="cnt">
+			<ul class="menu">
+				<li><a href="#">Взгляд</a></li>
+				<li><a href="#">Вкус</a></li>
+				<li><a href="#">Звук</a></li>
+				<li><a href="#">Ощущения</a></li>
+				<li><a href="#">Запах</a></li>
+				<li><a href="#">Интуиция</a></li>
+			</ul>
+		</div>
+	</div>
+</header>
 	
 	<main></main>
 	
