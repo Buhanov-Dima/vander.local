4833203f42c6b55f212680585a022c7487825318
piterrus0102 <piterrus@mail.ru>
2018-12-18 21:01:02 +0300

    добавил 3 документа

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+	<head>
+	 <meta charset="UTF-8">
+	<title>test</title>
+	<link rel="stylesheet" href="/css/style.css">
+	</head>
+	<body>
+		<header>
+		</header>
+		<aside>
+		</aside>
+		<main>
+		</main>
+		<aside>
+		</aside>
+		<footer>
+		</footer>
+		<script src="/js/main.js"></script>
+	</body>
+</html>
