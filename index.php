3b27d33f0ef8a53251f175a3dc638d2712a297e6
Buhanov-Dima <35217770+Buhanov-Dima@users.noreply.github.com>
2018-12-18 21:04:22 +0300

    fx index - style

--- index.php
+++ index.php
@@ -1,21 +1,33 @@
 <!doctype html>
 <html>
-	<head>
-	 <meta charset="UTF-8">
+<head>
+	<meta charset="UTF-8">
 	<title>test</title>
 	<link rel="stylesheet" href="/css/style.css">
-	</head>
-	<body>
-		<header>
-		</header>
-		<aside>
-		</aside>
-		<main>
-		</main>
-		<aside>
-		</aside>
-		<footer>
-		</footer>
-		<script src="/js/main.js"></script>
+</head>
+<body>
+	<header>
+		<div class="cnt">
+			<div class="logo">
+				<img src="img/logo.png" alt="">
+			</div>
+			<div class="main_menu">
+				<ul class="menu">
+					<a href="#"><li>Взгляд</li></a>
+					<a href="#"><li>Вкус</li></a>
+					<a href="#"><li>Звук</li></a>
+					<a href="#"><li>Ощущения</li></a>
+					<a href="#"><li>Запах</li></a>
+					<a href="#"><li>Интуиция</li></a>
+				</ul>
+			</div>
+		</div>
+	</header>
+	
+	<main></main>
+	
+	<footer></footer>
+
+	<script src="/js/main.js"></script>
 	</body>
 </html>
