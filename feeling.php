23518ea72709a874fb4e958d7a6fd35e0b6453e3
piterrus0102 <piterrus@mail.ru>
2018-12-23 16:39:33 +0300

    Закончил  с версткой. Щас начну разбираться со всякими мелкими проблемами.

---
+++ feeling.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Van Der Q</title>
+	<link rel="stylesheet" href="/css/style.css">
+</head>
+<body class="Feeling">
+<header>
+	<div class="cnt">
+		<div class="logo">
+			<img src="img/Main/logo.png" alt="">
+		</div>
+	</div>
+	<div class="main_menu">
+		<div class="cnt">
+			<ul class="menu">
+				<li><a href="#">Взгляд</a></li>
+				<li><a href="#">Вкус</a></li>
+				<li><a href="#">Звук</a></li>
+				<li><a href="#">Ощущения</a></li>
+				<li><a href="#">Запах</a></li>
+				<li><a href="#">Интуиция</a></li>
+			</ul>
+		</div>
+	</div>
+</header>
+<main>
+	<div class="cnt7">
+		<div class="bag_taste">
+			<div id="hook">
+				<img src="img/Main/hook.png" alt="">
+			</div>
+			<div id="bag_el">
+				<img src="img/Main/bag.png" alt="">
+			</div>
+		</div>
+		<div class="picture_top_feeling">
+			<img src="img/Feeling/picture_top.png" alt="">
+		</div>
+		<div class="clock_feeling">
+			<img src="img/Feeling/clock.png" alt="">
+		</div>
+		<div class="paper_smell">
+			<div class="paper">
+				<img src="img/Smell/piece_paper.png" alt="">
+				<div class="first_block">
+					<div class="headik"><h3>Первый бокс</h3></div>
+					<div class="cnt_div">
+						<div class="photos_div">
+							<img src="img/Smell/photo1.png" alt="">
+						</div>
+						<p>Текст внутри первого блока.</p>
+					</div>
+				</div>
+				<div class="second_block">
+					<div class="headik"><h3>Второй бокс</h3></div>
+					<div class="cnt_div">
+						<div class="photos_div">
+							<img src="img/Smell/photo2.png" alt="">
+						</div>
+						<p>Текст внутри второго блока.</p>
+					</div>
+				</div>
+				<div class="third_block">
+					<div class="headik"><h3>Третий бокс</h3></div>
+					<div class="cnt_div">
+						<div class="photos_div">
+							<img src="img/Smell/photo3.png" alt="">
+						</div>
+						<p>Текст внутри третьего блока.</p>	
+					</div>						
+				</div>
+			</div>
+			<div class="pensil_smell">
+				<img src="img/Smell/pensil.png" alt="">
+			</div>		
+		</div>
+		<div class="field">
+			<img src="img/Feeling/field.png" alt="">
+		</div>
+		<div class="leonardo">
+			<img src="img/Feeling/leonardo.png" alt="">
+		</div>
+		<div class="colors">
+			<img src="img/Feeling/colors.png" alt="">
+		</div>
+	</div>
+</main>
+	
+<footer></footer>
+
+<script src="/js/main.js"></script>
+</body>
+</html>
---
+++ view.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Van Der Q</title>
+	<link rel="stylesheet" href="/css/style.css">
+</head>
+<body class="View">
+<header>
+	<div class="cnt">
+		<div class="logo">
+			<img src="img/Main/logo.png" alt="">
+		</div>
+	</div>
+	<div class="main_menu">
+		<div class="cnt">
+			<ul class="menu">
+				<li><a href="#">Взгляд</a></li>
+				<li><a href="#">Вкус</a></li>
+				<li><a href="#">Звук</a></li>
+				<li><a href="#">Ощущения</a></li>
+				<li><a href="#">Запах</a></li>
+				<li><a href="#">Интуиция</a></li>
+			</ul>
+		</div>
+	</div>
+</header>
+<main>
+	<div class="cnt8">
+		<div class="bag_taste">
+			<div id="hook">
+				<img src="img/Main/hook.png" alt="">
+			</div>
+			<div id="bag_el">
+				<img src="img/Main/bag.png" alt="">
+			</div>
+		</div>
+		<div class="pipe_view">
+			<img src="img/View/pipe.png" alt="">
+		</div>
+		<div class="photo_view">
+			<img src="img/View/photo.png" alt="">
+		</div>
+	</div>
+</main>
+	
+<footer></footer>
+
+<script src="/js/main.js"></script>
+</body>
+</html>
